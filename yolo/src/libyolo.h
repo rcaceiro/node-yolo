4d4da0565513273db1b49ca6eecdb77993afd4ab
rcaceiro <r.caceiro@hotmail.com>
2018-04-15 18:35:20 +0100

    new API

--- yolo/src/libyolo.h
+++ yolo/src/libyolo.h
@@ -1,38 +1,23 @@
 #ifndef LIBYOLO_H
 #define LIBYOLO_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "darknet.h"
-#include "option_list.h"
-#include "image.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void *yolo_handle;
-
 typedef struct
 {
- char name[32];
- int left;
- int right;
- int top;
- int bottom;
- float prob;
-}detection_info;
+ detection *detection;
+ int num_boxes;
+}yolo_detection;
 
-yolo_handle yolo_init(char *darknet_path, char *datacfg, char *cfgfile, char *weightfile);
+network *yolo_init(char *darknet_path, char *datacfg, char *cfgfile, char *weightfile);
 
-void yolo_cleanup(yolo_handle handle);
+void yolo_cleanup(network *net);
 
-detection_info **yolo_detect(yolo_handle handle, image im, float thresh, float hier_thresh, int *num);
-
-detection_info **yolo_test(yolo_handle handle, char *filename, float thresh, float hier_thresh, int *num, float **feature_map, int *map_size);
-
+yolo_detection *yolo_detect(network *net, image im, float thresh, float hier_thresh, int *num);
 #ifdef __cplusplus
 }
 #endif
