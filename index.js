c3a428290ba47e80b25778b4e88115c632f98faf
rcaceiro <r.caceiro@hotmail.com>
2018-04-17 13:18:42 +0100

    ...

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let addons = require('/Users/rcaceiro/CloudStation/IPLeiria/3year/Project/NodeYoloJS/build/Debug/nodeyolojs');
-let yolo = new addons.Yolo("./darknet", "./darknet/cfg/coco.data", "./darknet/cfg/tiny-yolo.cfg", "tiny-yolo.weights");
+let yolo = new addons.Yolo("./darknet", "./darknet/cfg/coco.data", "./darknet/cfg/yolov3.cfg", "yolov3.weights");
 console.dir(yolo.detect());
