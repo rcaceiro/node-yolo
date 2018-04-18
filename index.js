c5df264d97f065988e7b26c5edf2f1650d130773
rcaceiro <r.caceiro@hotmail.com>
2018-04-19 00:12:11 +0100

    change parametrs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let addons = require('/Users/rcaceiro/CloudStation/IPLeiria/3year/Project/NodeYoloJS/build/Debug/nodeyolojs');
-let yolo = new addons.Yolo("./darknet", "./cfg/coco.data", "./cfg/yolov3.cfg", "../yolov3.weights");
-console.log(yolo.detect("darknet/data/dog.jpg"));
+let yolo = new addons.Yolo("./darknet", "./cfg/coco.data", "./cfg/yolov3.cfg", "../yolov3-416.weights");
+let yolo1 = new addons.Yolo("./darknet", "./cfg/coco.data", "./cfg/yolov2-tiny.cfg", "../yolov2-tiny.weights");
+console.log(yolo.detect("darknet/data/kite.jpg"));
+console.log(yolo1.detect("darknet/data/kite.jpg"));
