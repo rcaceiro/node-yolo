5a8f6ccd09c3ecc117ce1db210594c5e591eef81
rcaceiro <r.caceiro@hotmail.com>
2018-04-19 15:03:53 +0100

    changed image to detect

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let addons = require('/Users/rcaceiro/CloudStation/IPLeiria/3year/Project/NodeYoloJS/build/Debug/nodeyolojs');
 let yolo = new addons.Yolo("./darknet", "./cfg/coco.data", "./cfg/yolov3.cfg", "../yolov3-416.weights");
 // let yolo1 = new addons.Yolo("./darknet", "./cfg/coco.data", "./cfg/yolov2-tiny.cfg", "../yolov2-tiny.weights");
-let obj = yolo.detect("darknet/data/kite.jpg");
+let obj = yolo.detect("darknet/data/dog.jpg");
 console.log(JSON.stringify(obj));
 // let obj1=yolo1.detect("darknet/data/kite.jpg");
 // console.log(JSON.stringify(obj1));
