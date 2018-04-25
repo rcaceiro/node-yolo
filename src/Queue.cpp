5bda6074d85e37ce0dd8390c7e4dfb971c37f5b1
rcaceiro <r.caceiro@hotmail.com>
2018-04-25 03:05:48 +0100

    Remove namespace if errors occurred check this http://www.cplusplus.com/forum/beginner/196863/

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -1,137 +1,134 @@
 #include <utility>
 #include "Queue.h"
 
-namespace my
+// Default Constructor
+template<typename Type> Queue<Type>::Queue(): head{nullptr}, tail{nullptr}, theSize{0}
 {
- // Default Constructor
- template<typename Type> Queue<Type>::Queue(): head{nullptr}, tail{nullptr}, theSize{0}
+}
+
+// Copy Constructor
+template<typename Type> Queue<Type>::Queue(const Queue &rhs)
+     : head{nullptr}, tail{nullptr}, theSize{0}
+{
+ for(auto n=rhs.head; n != nullptr; n=n->next)
  {
+  push_back(n->data, tail);
+ }
+}
+
+// Move Constructor
+template<typename Type> Queue<Type>::Queue(Queue &&rhs)
+{
+ this->swap(rhs);
+}
+
+// Copy/Move Assignment
+template<typename Type> Queue<Type> &Queue<Type>::operator=(Queue rhs)
+{
+ return this->swap(rhs);
+}
+
+// Destructor
+template<typename Type> Queue<Type>::~Queue()
+{
+ clear();
+}
+
+// Public Function to add an element
+template<typename Type> void Queue<Type>::push(const Type &x)
+{
+ push_back(x, tail);
+}
+
+// Internal Function to add an element
+template<typename Type> void Queue<Type>::push_back(const Type &x, QueueNode *t)
+{
+ if(t == nullptr)
+ {
+  tail=new QueueNode{x};
+  head=tail;
+ }
+ else
+ {
+  tail->next=new QueueNode{x};
+  tail=tail->next;
  }
 
- // Copy Constructor
- template<typename Type> Queue<Type>::Queue(const Queue &rhs)
-      : head{nullptr}, tail{nullptr}, theSize{0}
- {
-  for(auto n=rhs.head; n != nullptr; n=n->next)
-  {
-   push_back(n->data, tail);
-  }
+ theSize++;
+}
+
+// Public Function to remove an element
+template<typename Type> void Queue<Type>::pop()
+{
+ pop_front(head);
+}
+
+// Internal Function to remove an element
+template<typename Type> void Queue<Type>::pop_front(QueueNode *h)
+{
+ if(h == nullptr)
+ {; // If the container is empty, do nothing
  }
-
- // Move Constructor
- template<typename Type> Queue<Type>::Queue(Queue &&rhs)
+ else if(h->next == nullptr)
  {
-  this->swap(rhs);
+  tail=nullptr;
+  head=nullptr;
+  delete h;
+  theSize--;
  }
-
- // Copy/Move Assignment
- template<typename Type> Queue<Type> &Queue<Type>::operator=(Queue rhs)
+ else
  {
-  return this->swap(rhs);
+  head=h->next;
+  delete h;
+  theSize--;
  }
+}
 
- // Destructor
- template<typename Type> Queue<Type>::~Queue()
- {
-  clear();
- }
+// Return modifiable reference to the front
+template<typename Type> Type &Queue<Type>::front()
+{
+ return head->data;
+}
 
- // Public Function to add an element
- template<typename Type> void Queue<Type>::push(const Type &x)
- {
-  push_back(x, tail);
- }
+// Return modifiable reference to the back
+template<typename Type> Type &Queue<Type>::back()
+{
+ return tail->data;
+}
 
- // Internal Function to add an element
- template<typename Type> void Queue<Type>::push_back(const Type &x, QueueNode *t)
- {
-  if(t == nullptr)
-  {
-   tail=new QueueNode{x};
-   head=tail;
-  }
-  else
-  {
-   tail->next=new QueueNode{x};
-   tail=tail->next;
-  }
+// Return theSize
+template<typename Type> int Queue<Type>::size() const
+{
+ return theSize;
+}
 
-  theSize++;
- }
+// Test whether the container is empty
+template<typename Type> bool Queue<Type>::empty() const
+{
+ return theSize == 0;
+}
 
- // Public Function to remove an element
- template<typename Type> void Queue<Type>::pop()
+// Member-wise Swap Function
+template<typename Type> Queue<Type> &Queue<Type>::swap(Queue<Type> &that)
+{
+ using std::swap;
+ swap(head, that.head);
+ swap(tail, that.tail);
+ swap(theSize, that.theSize);
+
+ return *this;
+}
+
+// Internal Function to delete all contents - Called by Destructor
+template<typename Type> void Queue<Type>::clear()
+{
+ while(head != nullptr)
  {
   pop_front(head);
  }
+}
 
- // Internal Function to remove an element
- template<typename Type> void Queue<Type>::pop_front(QueueNode *h)
- {
-  if(h == nullptr)
-  {; // If the container is empty, do nothing
-  }
-  else if(h->next == nullptr)
-  {
-   tail=nullptr;
-   head=nullptr;
-   delete h;
-   theSize--;
-  }
-  else
-  {
-   head=h->next;
-   delete h;
-   theSize--;
-  }
- }
-
- // Return modifiable reference to the front
- template<typename Type> Type &Queue<Type>::front()
- {
-  return head->data;
- }
-
- // Return modifiable reference to the back
- template<typename Type> Type &Queue<Type>::back()
- {
-  return tail->data;
- }
-
- // Return theSize
- template<typename Type> int Queue<Type>::size() const
- {
-  return theSize;
- }
-
- // Test whether the container is empty
- template<typename Type> bool Queue<Type>::empty() const
- {
-  return theSize == 0;
- }
-
- // Member-wise Swap Function
- template<typename Type> Queue<Type> &Queue<Type>::swap(Queue<Type> &that)
- {
-  using std::swap;
-  swap(head, that.head);
-  swap(tail, that.tail);
-  swap(theSize, that.theSize);
-
-  return *this;
- }
-
- // Internal Function to delete all contents - Called by Destructor
- template<typename Type> void Queue<Type>::clear()
- {
-  while(head != nullptr)
-  {
-   pop_front(head);
-  }
- }
-
- template<typename Type> void swap(Queue<Type> &lhs, Queue<Type> &rhs)
- {
-  lhs.swap(rhs);
- }
+template<typename Type> void swap(Queue<Type> &lhs, Queue<Type> &rhs)
+{
+ lhs.swap(rhs);
 }
--- src/Queue.h
+++ src/Queue.h
@@ -4,77 +4,76 @@
 
 #ifndef NODEYOLOJS_QUEUE_H
 #define NODEYOLOJS_QUEUE_H
-namespace my
+
+template<typename Type> class Queue
 {
- template<typename Type> class Queue
+private:
+ // Node Structure
+ struct QueueNode
  {
- private:
-  // Node Structure
-  struct QueueNode
+  Type data;
+  QueueNode *next;
+
+  QueueNode(const Type &x) : data{x}, next{nullptr}
   {
-   Type data;
-   QueueNode *next;
+  }
 
-   QueueNode(const Type &x) : data{x}, next{nullptr}
-   {
-   }
-
-   QueueNode(Type &x, QueueNode *n) : data{x}, next{n}
-   {
-   }
-  };
-
- public:
-
-  // Default Constructor
-  Queue();
-
-  // Copy Constructor
-  Queue(const Queue &rhs);
-
-  // Move Constructor
-  Queue(Queue &&rhs);
-
-  // Copy/Move Assignment
-  Queue &operator=(Queue rhs);
-
-  // Destructor
-  ~Queue();
-
-  // Public Function to add an element
-  void push(const Type &x);
-
-  // Public Function to remove an element
-  void pop();
-
-  // Member-wise Swap Function
-  Queue &swap(Queue &that);
-
-  // Return theSize
-  int size() const;
-
-  // Test whether the container is empty
-  bool empty() const;
-
-  // Return modifiable reference to the first element
-  Type &front();
-
-  // Return modifiable reference to the last element
-  Type &back();
-
- private:
-  // Internal Function to clear all contents - Called by Destructor
-  void clear();
-
-  // Internal Function to add an element
-  void push_back(const Type &x, QueueNode *t);
-
-  // Internal Function to remove an element
-  void pop_front(QueueNode *h);
-
-  QueueNode *head;
-  QueueNode *tail;
-  int theSize;
+  QueueNode(Type &x, QueueNode *n) : data{x}, next{n}
+  {
+  }
  };
-}
+
+public:
+
+ // Default Constructor
+ Queue();
+
+ // Copy Constructor
+ Queue(const Queue &rhs);
+
+ // Move Constructor
+ Queue(Queue &&rhs);
+
+ // Copy/Move Assignment
+ Queue &operator=(Queue rhs);
+
+ // Destructor
+ ~Queue();
+
+ // Public Function to add an element
+ void push(const Type &x);
+
+ // Public Function to remove an element
+ void pop();
+
+ // Member-wise Swap Function
+ Queue &swap(Queue &that);
+
+ // Return theSize
+ int size() const;
+
+ // Test whether the container is empty
+ bool empty() const;
+
+ // Return modifiable reference to the first element
+ Type &front();
+
+ // Return modifiable reference to the last element
+ Type &back();
+
+private:
+ // Internal Function to clear all contents - Called by Destructor
+ void clear();
+
+ // Internal Function to add an element
+ void push_back(const Type &x, QueueNode *t);
+
+ // Internal Function to remove an element
+ void pop_front(QueueNode *h);
+
+ QueueNode *head;
+ QueueNode *tail;
+ int theSize;
+};
+
 #endif //NODEYOLOJS_QUEUE_H
