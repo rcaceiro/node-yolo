9cf18297d372a585b45272683361fa4e63b7aaec
rcaceiro <r.caceiro@hotmail.com>
2018-04-25 23:38:54 +0100

    Merge

--- src/module.h
+++ src/module.h
@@ -9,6 +9,7 @@ class Yolo
 {
 public:
  static napi_value Init(napi_env env, napi_value exports);
+
  static void Destructor(napi_env env, void *nativeObject, void *finalize_hint);
 
  void mutex_lock();
@@ -19,8 +20,11 @@ public:
 private:
  explicit Yolo(char *working_directory, char *datacfg, char *cfgfile, char *weightfile);
  ~Yolo();
+
  static napi_value New(napi_env env, napi_callback_info info);
+
  static napi_value Detect(napi_env env, napi_callback_info info);
+
  static napi_ref constructor;
 
  pthread_mutex_t mutex;
