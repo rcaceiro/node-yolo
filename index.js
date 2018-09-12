8e03c8a6bbed4f6346fa77bf711fcd91ed79a863
Ruben Caceiro <caceiro.ruben@gmail.com>
2018-09-12 22:32:07 +0100

    add support to imagemagick to check if image is corrupted

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-module.exports = require(__dirname + '/build/Release/nodeyolojs').Yolo;
+const YoloModule = require(__dirname + '/build/Release/nodeyolojs').Yolo;
+const imagemagick = require('imagickal');
+
+class Yolo
+{
+ constructor(working_dir, dataset, configuration_file, weights)
+ {
+  this.yolo = new YoloModule(working_dir, dataset, configuration_file, weights);
+ }
+
+ detect(image_path)
+ {
+  return new Promise((resolve, reject) =>
+  {
+   imagemagick.identify(image_path, true).then(() =>
+   {
+    this.yolo.detect(image_path).then(detections =>
+    {
+     resolve(detections);
+    })
+    .catch(error =>
+    {
+     reject(error);
+    });
+   })
+   .catch(() =>
+   {
+    reject("Image " + image_path + " is corrupted.");
+   });
+  });
+ }
+}
+
+module.exports = Yolo;
