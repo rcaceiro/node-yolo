660d0db27be65c993a1c680115c8a7f61819ab97
rcaceiro <r.caceiro@hotmail.com>
2018-10-10 11:13:04 +0100

    fix providing weights location

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let yolo_addon = require(__dirname + '/build/Debug/nodeyolo').Yolo;
 
-let obj = new yolo_addon("./darknet", "./cfg/coco.data", "./cfg/yolov3-spp.cfg", ".weights/yolov3-spp.weights");
+let obj = new yolo_addon("./darknet", "./cfg/coco.data", "./cfg/yolov3-spp.cfg", "../weights/yolov3-spp.weights");
 
 obj.detect("./darknet/data/dog.jpg")
     .then((detections) => {
