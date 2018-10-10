04f33902d48ff8867824775cb66af25d10eb75fd
rcaceiro <r.caceiro@hotmail.com>
2018-10-10 23:21:06 +0100

    fix method name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let yolo_addon = require(__dirname + '/build/Debug/nodeyolo').Yolo;
 
 let obj = new yolo_addon("./darknet", "./cfg/coco.data", "./cfg/yolov3-spp.cfg", "../weights/yolov3-spp.weights");
 
-obj.detect("./darknet/data/dog.jpg")
+obj.detectImage("./darknet/data/dog.jpg")
     .then((detections) => {
         console.log(JSON.stringify(detections));
     })
