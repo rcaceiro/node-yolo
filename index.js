f0ebb1c9603aefc0ac60d31deb77baf492f6a395
rcaceiro <r.caceiro@hotmail.com>
2018-10-31 15:17:24 +0000

    removed common header file. Some chnages on CMakeLists Some dev changes on index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ let yolo_addon = require(__dirname + '/build/Debug/nodeyolo').Yolo;
 
 let obj = new yolo_addon("./darknet", "./cfg/coco.data", "./cfg/yolov3-spp.cfg", "../weights/yolov3-spp.weights");
 
-// obj.detectImage("./darknet/data/kite.jpg")
-//     .then((detections) => {
-//         console.log(JSON.stringify(detections));
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
-
-obj.detectVideo("./data/crowd.mp4")
+obj.detectImage("./darknet/data/kite.jpg")
     .then((detections) => {
         console.log(JSON.stringify(detections));
     })
     .catch((error) => {
         console.error(error);
     });
+
+// obj.detectVideo("./data/crowd.mp4")
+//     .then((detections) => {
+//         console.log(JSON.stringify(detections));
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     });
