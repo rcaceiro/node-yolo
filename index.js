ae947ac8c9ece628ddac5eaf976f8dcccc5a2bde
rcaceiro <r.caceiro@hotmail.com>
2018-11-01 13:10:42 +0000

    improved for several debug porposes

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 //module.exports = require(__dirname + '/build/Release/nodeyolo').Yolo;
+let fs = require('fs');
 
-let yolo_addon = require(__dirname + '/build/Debug/nodeyolo').Yolo;
-// let yolo_addon = require(__dirname + '/build/Release/nodeyolo').Yolo;
+let yolo_addon = undefined;
+if (fs.existsSync(__dirname + '/build/Debug/nodeyolo.node')) {
+    yolo_addon = require(__dirname + '/build/Debug/nodeyolo').Yolo;
+}
+else if (fs.existsSync(__dirname + '/build/Release/nodeyolo.node')) {
+    yolo_addon = require(__dirname + '/build/Release/nodeyolo').Yolo;
+}
+else {
+    return;
+}
 
 let obj = new yolo_addon("./darknet", "./cfg/coco.data", "./cfg/yolov3-spp.cfg", "../weights/yolov3-spp.weights");
 
-obj.detectImage("./darknet/data/kite.jpg")
-    .then((detections) => {
-        console.log(JSON.stringify(detections));
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+let isMac = process.platform === "darwin";
 
-// obj.detectVideo("./data/crowd.mp4")
-//     .then((detections) => {
-//         console.log(JSON.stringify(detections));
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
+if (isMac) {
+    obj.detectImage("./darknet/data/kite.jpg")
+        .then((detections) => {
+            console.log(JSON.stringify(detections));
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+else {
+    if (process.argv[2] !== undefined) {
+        obj.detectVideo(process.argv[2])
+            .then((detections) => {
+                console.log(JSON.stringify(detections));
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+}
